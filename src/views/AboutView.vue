6ea73c4a66cdcc58ba78bc9dfbf152b02b84168f
baris.gungor@etiya.com <baris.gungor@etiya.com>
2022-06-06 12:02:50 +0300

    first deploy

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,35 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about pa-6">
+    <h1>About Vuetify Todo</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur eos
+      sequi eaque laborum cumque, porro sed consequuntur quasi recusandae cum
+      eligendi ipsa sunt, ratione sapiente accusamus voluptas? Ipsa, eligendi
+      ut.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur eos
+      sequi eaque laborum cumque, porro sed consequuntur quasi recusandae cum
+      eligendi ipsa sunt, ratione sapiente accusamus voluptas? Ipsa, eligendi
+      ut.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur eos
+      sequi eaque laborum cumque, porro sed consequuntur quasi recusandae cum
+      eligendi ipsa sunt, ratione sapiente accusamus voluptas? Ipsa, eligendi
+      ut.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur eos
+      sequi eaque laborum cumque, porro sed consequuntur quasi recusandae cum
+      eligendi ipsa sunt, ratione sapiente accusamus voluptas? Ipsa, eligendi
+      ut.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur eos
+      sequi eaque laborum cumque, porro sed consequuntur quasi recusandae cum
+      eligendi ipsa sunt, ratione sapiente accusamus voluptas? Ipsa, eligendi
+      ut.
+    </p>
   </div>
 </template>
